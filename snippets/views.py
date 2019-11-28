a37445977b7672605e8c6d0c6620c8acb7fc82fe
Florian Kempenich <Flori@nKempenich.com>
2019-11-28 15:55:51 +0000

    Add view to display Highlighted Snippet as HTML

--- snippets/views.py
+++ snippets/views.py
@@ -1,10 +1,13 @@
 from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 from django.contrib.auth.models import User
 from rest_framework import mixins, generics, permissions
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework.renderers import StaticHTMLRenderer
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
@@ -24,6 +27,15 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = [IsOwnerOrReadOnly]
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 class UserList(generics.ListAPIView):

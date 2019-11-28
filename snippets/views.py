aee02a2a63ea2af8381092eccea82953f5746e1b
Florian Kempenich <Flori@nKempenich.com>
2019-11-28 15:02:43 +0000

    Save logged-in user as owner when creating a snippet

--- snippets/views.py
+++ snippets/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404
 
 from django.contrib.auth.models import User
-from rest_framework import mixins, generics
+from rest_framework import mixins, generics, permissions
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
@@ -10,11 +10,16 @@ from snippets.serializers import SnippetSerializer, UserSerializer
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class UserList(generics.ListAPIView):

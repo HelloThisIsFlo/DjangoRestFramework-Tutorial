6e3df7d8b418a3e8c8f1f3b39230da6e96390d8f
Florian Kempenich <Flori@nKempenich.com>
2019-11-28 15:45:57 +0000

    Add root page

--- snippets/views.py
+++ snippets/views.py
@@ -2,6 +2,9 @@ from django.http import Http404
 
 from django.contrib.auth.models import User
 from rest_framework import mixins, generics, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
@@ -31,3 +34,11 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format),
+    })
